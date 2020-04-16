ad5692a76781031df075dfa588050f20a635e13e
Hansen Artajaya <artajayahansen12@gmail.com>
2020-04-16 23:54:01 +0700

    Init commit

---
+++ TypePractice/Extension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TypePractice
+{
+    public static class Extension
+    {
+        public static string Capitalize(this string text)
+        {
+            var characters = text.ToCharArray();
+            characters[0] = char.ToUpper(characters[0]);
+            return new string(characters);
+        }
+    }
+}
